9ddf618fef01d29214b62d5cc436a3dd5b383f89
Hokure04 <hokure.04@gmail.com>
2024-05-03 00:41:44 +0300

    linear approximation func full completed

--- lab4.f90
+++ lab4.f90
@@ -51,9 +51,134 @@ contains
     end subroutine
 end module KeyboardReader
 
+module CorrelationCoefficient
+    implicit none
+
+contains
+    subroutine calc_correlation(x, y, n)
+        real, allocatable, intent(in) :: x(:), y(:)
+        integer, intent(in) :: n
+        real :: r, mean_x, mean_y, sum_x, sum_y, numerator
+        integer :: i
+        sum_x = 0
+        sum_y = 0
+        do i=1,n
+            sum_x = sum_x + x(i)
+            sum_y = sum_y + y(i)
+        end do
+        mean_x = sum_x/n
+        mean_y = sum_y/n
+        sum_x = 0
+        sum_y = 0
+        do i=1,n
+            sum_x = sum_x + (x(i)-mean_x)**2
+            sum_y = sum_y + (y(i)-mean_y)**2
+            numerator = numerator + (x(i) - mean_x)*(y(i) - mean_y)
+        end do
+        r = numerator/sqrt(sum_x*sum_y)
+        print *, 'Correlation coefficient: ', r
+    end subroutine
+end module CorrelationCoefficient
+
+module DeterminationCoefficient
+    implicit none
+    
+contains
+
+    subroutine calc_determination(y, fi, n)
+        real, allocatable, intent(in) :: y(:), fi(:)
+        integer, intent(in) :: n
+        real :: mean_fi, numerator, denominator, determination
+        integer :: i
+        mean_fi = 0
+        do i =1,n
+            mean_fi = mean_fi + fi(i)
+        end do
+        mean_fi = mean_fi/n
+        
+        numerator = 0
+        denominator = 0
+        do i=1,n
+            numerator = numerator + (y(i) - fi(i))**2
+            denominator = denominator + (y(i)-mean_fi)**2
+        end do
+        determination = 1 - numerator/denominator
+        print *,'Determination coefficient: ', determination
+    end subroutine
+end module DeterminationCoefficient
+
+module StandatrDeviation
+    implicit none
+    
+contains
+
+    subroutine calc_deviation(y, fi, n)
+        real, allocatable, intent(in) :: y(:), fi(:)
+        integer, intent(in) :: n
+        real :: sum, deviation
+        integer :: i
+        do i=1,n
+            sum = sum + (fi(i) - y(i))**2
+        end do
+        deviation = sqrt(sum/n)
+        print *, 'Standart deviation: ', deviation
+    end subroutine
+end module StandatrDeviation
+
+module LinearAproximation
+    use CorrelationCoefficient
+    use DeterminationCoefficient
+    use StandatrDeviation
+    implicit none
+    
+contains
+    subroutine linear_approximation(x,y,n)
+        real, allocatable, intent(in) :: x(:), y(:)
+        integer, intent(in) :: n
+        real :: sx, sxx, sy, sxy, delta, delta1, delta2, a, b
+        real, allocatable :: P_x(:), e_i(:)
+        integer :: i
+        sx = 0.0
+        sxx = 0.0
+        sy = 0.0
+        sxy = 0.0
+        do i =1,n
+            sx = sx+x(i)
+            sxx = sxx+x(i)**2
+            sy = sy+y(i)
+            sxy = sxy + y(i)*x(i)
+        end do
+        print *, 'SX:', sx, 'SXX:', sxx, 'SY:', sy, 'SXY', sxy
+        delta = sxx*n - sx*sx
+        delta1 = sxy*n -sx*sy
+        delta2 = sxx*sy - sx*sxy
+        print *, 'delta:', delta, 'delta1:', delta1, 'delta2:', delta2
+        if(delta /= 0) then
+            a = delta1/delta
+            b = delta2/delta
+            print *, 'Value a:', a
+            print *, 'Value b:', b
+            allocate(P_x(n))
+            allocate(e_i(n))
+            do i=1,n
+                P_x(i) = a*x(i)+b
+                e_i(i) = P_x(i) - y(i)
+            end do
+            print *, 'P_x:',P_x
+            print *,'e_i:', e_i
+            call calc_correlation(x,y,n)
+            call calc_determination(y, P_x, n)
+            call calc_deviation(y,P_x,n)
+        else
+            print *, 'ERROR: Delta parameter is 0, but we cannnot divide by zero'
+        end if
+    end subroutine
+end module LinearAproximation
+
 program main
     use FileReader
     use KeyboardReader
+    use LinearAproximation
     implicit none
     real, allocatable :: x(:), y(:)
     integer :: n
@@ -76,6 +201,7 @@ program main
                 print *, "y:", y
                 print *, "n:", n 
             end if
+            call linear_approximation(x,y,n)
         else if (func_number == 2) then
             call keyboard_input(x, y, n)
             print *, "x values:", x
