8ec32e32c9b219a5b576ca74cfd659405e225e27
Hokure04 <hokure.04@gmail.com>
2024-05-03 03:11:13 +0300

    quadratic approximation almost done

--- lab4.f90
+++ lab4.f90
@@ -174,11 +174,121 @@ contains
         end if
     end subroutine
 end module LinearAproximation
+module Matrix
+    implicit none
+    
+contains
+
+function determinant(matrix) result(det)
+    real, intent(in) :: matrix(:,:)
+    real :: det
+    det = matrix(1,1) * (matrix(2,2)*matrix(3,3) - matrix(2,3)*matrix(3,2)) &
+        - matrix(1,2) * (matrix(2,1)*matrix(3,3) - matrix(2,3)*matrix(3,1)) &
+        + matrix(1,3) * (matrix(2,1)*matrix(3,2) - matrix(2,2)*matrix(3,1))
+end function determinant
+
+subroutine inverse_matrix(matrix, inverse)
+    real, intent(in) :: matrix(:,:)
+    real, intent(out) :: inverse(:,:)
+    real :: det
+    ! integer :: i, j
+
+    det = determinant(matrix)
+    if (det == 0.0) then
+        print *, 'ERROR: The matrix is singular, inverse cannot be computed.'
+        return
+    endif
+
+    inverse(1,1) = (matrix(2,2)*matrix(3,3) - matrix(2,3)*matrix(3,2)) / det
+    inverse(1,2) = -(matrix(1,2)*matrix(3,3) - matrix(1,3)*matrix(3,2)) / det
+    inverse(1,3) = (matrix(1,2)*matrix(2,3) - matrix(1,3)*matrix(2,2)) / det
+    inverse(2,1) = -(matrix(2,1)*matrix(3,3) - matrix(2,3)*matrix(3,1)) / det
+    inverse(2,2) = (matrix(1,1)*matrix(3,3) - matrix(1,3)*matrix(3,1)) / det
+    inverse(2,3) = -(matrix(1,1)*matrix(2,3) - matrix(1,3)*matrix(2,1)) / det
+    inverse(3,1) = (matrix(2,1)*matrix(3,2) - matrix(2,2)*matrix(3,1)) / det
+    inverse(3,2) = -(matrix(1,1)*matrix(3,2) - matrix(1,2)*matrix(3,1)) / det
+    inverse(3,3) = (matrix(1,1)*matrix(2,2) - matrix(1,2)*matrix(2,1)) / det
+end subroutine inverse_matrix
+
+subroutine solve_linear_system(matrix, rhs, solution)
+    real, intent(in) :: matrix(:,:), rhs(:)
+    real, intent(out) :: solution(:)
+    real :: det
+    det = determinant(matrix)
+    
+    if (det == 0.0) then
+        print *, 'ERROR: The matrix of the system is degenerate, the solution is impossible'
+        return
+    endif
+    
+    solution(1) = (rhs(1)*(matrix(2,2)*matrix(3,3) - matrix(2,3)*matrix(3,2)) &
+        - matrix(1,2)*(rhs(2)*matrix(3,3) - matrix(2,3)*rhs(3)) &
+        + matrix(1,3)*(rhs(2)*matrix(3,2) - matrix(2,2)*rhs(3))) / det
+    
+    solution(2) = (matrix(1,1)*(rhs(2)*matrix(3,3) - matrix(2,3)*rhs(3)) &
+        - rhs(1)*(matrix(2,1)*matrix(3,3) - matrix(2,3)*matrix(3,1)) &
+        + matrix(1,3)*(matrix(2,1)*rhs(3) - rhs(2)*matrix(3,1))) / det
+    
+    solution(3) = (matrix(1,1)*(matrix(2,2)*rhs(3) - rhs(2)*matrix(3,2)) &
+        - matrix(1,2)*(matrix(2,1)*rhs(3) - rhs(2)*matrix(3,1)) &
+        + rhs(1)*(matrix(2,1)*matrix(3,2) - matrix(2,2)*matrix(3,1)) ) / det
+end subroutine solve_linear_system
+end module Matrix
+
+module QuadraticApproximation
+    use DeterminationCoefficient
+    use StandatrDeviation
+    use Matrix
+    implicit none
+    
+contains
+    
+    subroutine quadratic_approximation(x, y, n)
+        real, allocatable, intent(in) :: x(:), y(:)
+        integer, intent(in) :: n
+        real :: sx = 0, sx_2 = 0, sx_3 = 0, sx_4 = 0, sy = 0, sxy = 0, sx_2y = 0
+        real :: matrix(3,3)
+        real :: rhs(3)
+        real, allocatable :: a(:)
+        integer :: i
+
+        do i=1,n
+            sx = sx + x(i)
+            sx_2 = sx_2 + x(i)**2
+            sx_3 = sx_3 + x(i)**3
+            sx_4 = sx_4 + x(i)**4
+            sy = sy + y(i)
+            sxy = sxy + x(i)*y(i)
+            sx_2y = sx_2y + x(i)**2*y(i)
+        end do
+        print *, 'SX=', sx, 'SX_2=', sx_2, 'SX_3=', sx_3, 'SX_4=', sx_4
+        print *, 'SY=', sy, 'SXY=', sxy, 'SX_2Y=', sx_2y
+
+        matrix(1,1) = n
+        matrix(1,2) = sx
+        matrix(1,3) = sx_2
+        matrix(2,1) = sx
+        matrix(2,2) = sx_2
+        matrix(2, 3) = sx_3
+        matrix(3, 1) = sx_2
+        matrix(3, 2) = sx_3
+        matrix(3, 3) = sx_4
+
+        rhs(1) = sy
+        rhs(2) = sxy
+        rhs(3) = sx_2y
+
+        allocate(a(3))
+        call solve_linear_system(matrix, rhs, a)
+        print *, a
+    end subroutine
+end module
 
 program main
     use FileReader
     use KeyboardReader
     use LinearAproximation
+    use QuadraticApproximation
     implicit none
     real, allocatable :: x(:), y(:)
     integer :: n
@@ -192,7 +302,7 @@ program main
         if (func_number > 2 .or. func_number < 1) then
             print *, 'Error: Uncorrect input, try again'
         else if ( func_number == 1 ) then
-            call file_input('test.txt', x, y, n)
+            call file_input('present2.txt', x, y, n)
             if (n < 8 .or. n > 12) then
                 print *, 'Points count incorrect shold to be from 8 till 12'
             else
@@ -202,6 +312,7 @@ program main
                 print *, "n:", n 
             end if
             call linear_approximation(x,y,n)
+            call quadratic_approximation(x,y,n)
         else if (func_number == 2) then
             call keyboard_input(x, y, n)
             print *, "x values:", x
